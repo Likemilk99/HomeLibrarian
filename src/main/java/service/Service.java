289950647824afe00fa958e68dd241a66d76c0b4
likemilk99 <likemilk99@gmail.com>
2016-02-04 21:54:47 +0300

    First commit XML+ SQL

---
+++ src/main/java/service/Service.java
@@ -0,0 +1,8 @@
+package service;
+
+/**
+ * Testing
+ */
+
+public class Service {
+}
---
+++ src/test/java/frontendtesting/TestLogin.java
@@ -0,0 +1,7 @@
+package frontendtesting;
+
+/**
+ * Created by likemilk on 04.02.2016.
+ */
+public class TestLogin {
+}
